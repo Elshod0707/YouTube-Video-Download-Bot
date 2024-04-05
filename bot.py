5ba65d76bfb8f1b6feece0ed390d41598fdddf54
Tushar <165200812+1289108513@users.noreply.github.com>
2024-04-05 15:57:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL
+# ©️ Tushar 
 
 from pyrogram import Client, filters
 from pytube import YouTube
 import asyncio
 
 # Replace 'YOUR_API_ID', 'YOUR_API_HASH', and 'YOUR_BOT_TOKEN' with your actual values
-API_ID = ''
-API_HASH = ''
-BOT_TOKEN = ''
+API_ID = '21125888'
+API_HASH = 'b03021a37d433123ca5e3635036ba8db'
+BOT_TOKEN = '6692102608:AAHlwvDq5Qxe5T2EwQPqRlww66pjZ-wyt3Y'
 
 # Create a Pyrogram client
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
